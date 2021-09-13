73247ce290fcb56f1ef641eda303f541b0098528
Tiago Rocha <tigs4dev@gmail.com>
2021-09-12 22:54:26 -0300

    Layers

---
+++ backend/src/main/java/com/elroiech/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.elroiech.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.elroiech.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
