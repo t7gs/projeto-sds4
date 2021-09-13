2a3146ff718783227cfb359bfcf4d5e4ebdf3056
Tiago Rocha <tigs4dev@gmail.com>
2021-09-13 01:05:50 -0300

    Group by search

--- backend/src/main/java/com/elroiech/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/elroiech/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.elroiech.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elroiech.dsvendas.dto.SaleDTO;
+import com.elroiech.dsvendas.dto.SaleSucessDTO;
+import com.elroiech.dsvendas.dto.SaleSumDTO;
 import com.elroiech.dsvendas.service.SaleService;
 
 @RestController
@@ -23,6 +27,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/sucess-by-seller")
+	public ResponseEntity<List<SaleSucessDTO>> sucessGroupedBySeller(){
+		List<SaleSucessDTO> list = service.sucessGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 
